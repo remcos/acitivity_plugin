a416709f6b477e7693a7e6f25a07e2755c8ae3bd
remcos <snijders.remco@gmail.com>
2023-07-24 15:59:48 +0200

    Rename of class

--- src/android/ConnectSessionActivity.kt
+++ src/android/ConnectSessionActivity.kt
@@ -5,7 +5,7 @@ import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ConnectionActivity : AppCompatActivity() {   
+class ConnectSessionActivity : AppCompatActivity() {   
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
